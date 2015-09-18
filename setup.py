aa9cb2c84eb3a476b04819d26e075f20ac395f85
chenglong.zou <chenglong.zq@gmail.com>
2015-09-19 00:57:02 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kaggle',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Joana',
+    author_email='',
+    description=''
+)
